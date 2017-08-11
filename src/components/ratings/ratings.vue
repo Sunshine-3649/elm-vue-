81af0b14d2cb74ac21461e23f2e52c9b0ccdd03b
WangYangYang <364933712@@qq.com>
2017-08-11 11:31:38 +0800

    商品评价注释

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="ratings" ref="ratings">
+    <!--ratings内容区  -->
     <div class="ratings-content">
       <div class="overview">
+        <!--左侧综合评分区  -->
         <div class="overview-left">
           <h1 class="score">{{seller.score}}</h1>
           <div class="title">综合评分</div>
           <div class="rank">高于周边商家{{seller.rankRate}}%</div>
         </div>
+        <!--右侧评价详情区  -->
         <div class="overview-right">
           <div class="score-wrapper">
             <span class="title">服务态度</span>
@@ -24,26 +27,35 @@
           </div>
         </div>
       </div>
+      <!--分隔块  -->
       <split></split>
+      <!--switch块  -->
       <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType" :onlyContent="onlyContent"
                     :ratings="ratings"></ratingselect>
+      <!--评价列表  -->
       <div class="rating-wrapper">
         <ul>
-          <li v-for="rating in ratings" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
+          <li v-for="(rating, index) in ratings" :key="index" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
+            <!--左侧头像  -->
             <div class="avatar">
               <img width="28" height="28" :src="rating.avatar">
             </div>
+            <!--右侧内容  -->
             <div class="content">
+              <!--顶部用户姓名 -->
               <h1 class="name">{{rating.username}}</h1>
+              <!--星星评分  -->
               <div class="star-wrapper">
                 <star :size="24" :score="rating.score"></star>
                 <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
               </div>
               <p class="text">{{rating.text}}</p>
+              <!--推荐块  -->
               <div class="recommend" v-show="rating.recommend && rating.recommend.length">
                 <span class="icon-thumb_up"></span>
-                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+                <span class="item" v-for="(item, index) in rating.recommend" :key="index">{{item}}</span>
               </div>
+              <!--顶部时间  -->
               <div class="time">
                 {{rating.rateTime | formatDate}}
               </div>
@@ -79,6 +91,7 @@
       };
     },
     created() {
+      // 获取ratings的数据
       this.$http.get('/api/ratings').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
@@ -92,6 +105,7 @@
       });
     },
     methods: {
+      // 是否有要显示的内容
       needShow(type, text) {
         if (this.onlyContent && !text) {
           return false;
@@ -102,12 +116,14 @@
           return type === this.selectType;
         }
       },
+      // 父组件监听子组件的方法
       selectRating(type) {
         this.selectType = type;
         this.$nextTick(() => {
           this.scroll.refresh();
         });
       },
+      // 父组件监听子组件的方法
       toggleContent() {
         this.onlyContent = !this.onlyContent;
         this.$nextTick(() => {
@@ -115,6 +131,7 @@
         });
       }
     },
+    // 时间格式化
     filters: {
       formatDate(time) {
         let date = new Date(time);
@@ -148,6 +165,7 @@
         width: 137px
         border-right: 1px solid rgba(7, 17, 27, 0.1)
         text-align: center
+        // 设置iphone5的左侧的最小宽度
         @media only screen and (max-width: 320px)
           flex: 0 0 120px
           width: 120px
@@ -168,6 +186,7 @@
       .overview-right
         flex: 1
         padding: 6px 0 6px 24px
+        // 设置iphone5的右侧最小宽度，刚好放下内容
         @media only screen and (max-width: 320px)
           padding-left: 6px
         .score-wrapper
